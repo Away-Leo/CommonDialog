f6fdceba495193ca23dd84b53de2cc43fe718faf
liaoguowei <781720639@qq.com>
2017-09-29 16:13:44 +0800

    新增sweetdialog

--- demo/src/main/java/com/pingxun/commondialog/MainActivity.java
+++ demo/src/main/java/com/pingxun/commondialog/MainActivity.java
@@ -1,5 +1,6 @@
 package com.pingxun.commondialog;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
             new CommomDialog(this, com.pingxun.library.R.style.dialog, "对话框测试", (dialog, confirm) -> {
                 if(confirm){
                     Toast.makeText(this,"点击了确定",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this,SampleActivity.class));
                 }else{
                     Toast.makeText(this,"点击了取消",Toast.LENGTH_SHORT).show();
                 }
