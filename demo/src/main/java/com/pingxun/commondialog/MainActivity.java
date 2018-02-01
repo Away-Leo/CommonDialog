8faba566e85750d007d62187f9afa7adc97daa9f
liaoguowei <781720639@qq.com>
2018-02-01 14:17:43 +0800

    添加加载中依赖

--- demo/src/main/java/com/pingxun/commondialog/MainActivity.java
+++ demo/src/main/java/com/pingxun/commondialog/MainActivity.java
@@ -1,12 +1,15 @@
 package com.pingxun.commondialog;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.pingxun.library.commondialog.CommomDialog;
+import com.zyao89.view.zloading.ZLoadingDialog;
+import com.zyao89.view.zloading.Z_TYPE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +28,15 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(this,"点击了取消",Toast.LENGTH_SHORT).show();
                 }
             }).setTitle("提示").show();
+        });
+        Button buttonPanel2=(Button) findViewById(R.id.buttonPanel2);
+        buttonPanel2.setOnClickListener(view -> {
+            new ZLoadingDialog(MainActivity.this).setLoadingBuilder(Z_TYPE.STAR_LOADING)//设置类型
+                        .setLoadingColor(Color.BLACK)//颜色
+                        .setHintText("定位中...")
+                        .setCancelable(false)
+                        .setHintTextSize(16) // 设置字体大小 dp
+                        .setHintTextColor(Color.GRAY).show();
         });
     }
 }
